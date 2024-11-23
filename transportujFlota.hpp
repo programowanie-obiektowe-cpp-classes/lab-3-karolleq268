1a6fbba0e36e643c3c1ea0e9c0ca82757afa9051
karolleq268 <karolleq268@proton.me>
2024-11-23 12:25:38 +0100

    Update transportujFlota.hpp

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,21 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    Stocznia stocznia{};
+    unsigned int przetransportowano = 0;
+    unsigned int liczbaZaglowcow = 0;
+
+    while(przetransportowano < towar) {
+        Statek* statek = stocznia(); // tworzy losowo albo tankowiec albo zaglowiec
+
+        przetransportowano += statek->transportuj();
+
+        if (dynamic_cast<Zaglowiec*>(statek)) { // Sprawdza czy obiekt jest typu Zaglowiec
+            liczbaZaglowcow++; 
+        }
+        
+        delete statek;
+    }
+
+    return liczbaZaglowcow;
 }
